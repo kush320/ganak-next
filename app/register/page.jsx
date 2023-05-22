a9b723920df3cd7df607984a7f4f6dfbb125406b
Kushal koirala <koiralakushal444@gmail.com>
2023-05-22 18:19:04 +0545

    login and register are created

---
+++ app/register/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { Box, Card } from "@chakra-ui/react";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+        <Box height="150px" bgColor={"#081A51"} borderBottomLeftRadius={"20%"} borderBottomRightRadius={"20%"}  >
+        </Box>
+        <Card height="80%" width='30%'>
+
+        </Card>
+
+    </div>
+  );
+}
